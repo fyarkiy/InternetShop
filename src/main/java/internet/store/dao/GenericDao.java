b32b9456d74a1b545ac5004583b1077d7511423d
FYarkiy <flx_jb@yahoo.com>
2020-09-08 18:16:06 +0300

    change name of variable

--- src/main/java/internet/store/dao/GenericDao.java
+++ src/main/java/internet/store/dao/GenericDao.java
@@ -6,11 +6,11 @@ import java.util.Optional;
 public interface GenericDao<T, K> {
     T create(T item);
 
-    Optional<T> get(K id);
+    Optional<T> get(K itemId);
 
     List<T> getAll();
 
     T update(T item);
 
-    boolean delete(K item);
+    boolean delete(K itemId);
 }
